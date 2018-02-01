84ffc2517e6785e978c44c0fefe65ede96426c8d
Nikita Kapshtyk <viks2332@gmail.com>
2018-02-01 18:37:35 +0300

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp       = require('gulp'), // Подключаем Gulp
+var gulp         = require('gulp'), // Подключаем Gulp
 	sass         = require('gulp-sass'), //Подключаем Sass пакет,
+	pug 		 = require('gulp-pug');
 	browserSync  = require('browser-sync'), // Подключаем Browser Sync
 	concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
 	uglify       = require('gulp-uglifyjs'), // Подключаем gulp-uglifyjs (для сжатия JS)
@@ -17,6 +18,15 @@ gulp.task('sass', function(){ // Создаем таск Sass
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('pug',function buildHtml() {
+	return gulp.src('app/pug/index.pug')
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest('app'))
+		.pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
@@ -38,7 +48,7 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
 });
 
-gulp.task('css-libs', ['sass'], function() {
+gulp.task('css-libs', ['sass', 'pug'], function() {
 	return gulp.src('app/css/libs.css') // Выбираем файл для минификации
 		.pipe(cssnano()) // Сжимаем
 		.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
@@ -48,6 +58,7 @@ gulp.task('css-libs', ['sass'], function() {
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
 	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch('app/pug/*.pug', ['pug']);
 	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
